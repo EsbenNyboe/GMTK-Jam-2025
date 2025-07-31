68f65dcaf41efe1233c69cd3dad63311ba75f4a3
EsbenNyboeKanda <enj@kanda.dk>
2025-07-31 17:47:23 +0200

    Combine force and torque, and measure speed

--- main_scene.gd
+++ main_scene.gd
@@ -7,18 +7,23 @@ extends Node3D
 
 var start_height: float
 var is_above_start_height: bool
+var time_since_last_swing: float = 0
 
 func _ready() -> void:
 	start_height = swing_seat.position.y + 0.1
 
 func _physics_process(delta: float) -> void:
-#	if Input.is_action_pressed("swing"):
-#		swing_mount.apply_torque(Vector3.BACK * torque_amount)
-	
+	if Input.is_action_pressed("swing"):
+		swing_mount.apply_torque(Vector3.BACK * torque_amount)
+
 	if Input.is_action_pressed("swing"):
 		swing_seat.apply_force(Vector3.RIGHT * force_amount)
 	
+	time_since_last_swing += delta
 	if swing_seat.position.y > start_height:
 		is_above_start_height = true
 	else:
+		if is_above_start_height:
+			print(time_since_last_swing)
+			time_since_last_swing = 0
 		is_above_start_height = false
