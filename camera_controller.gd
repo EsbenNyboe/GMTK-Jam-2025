efd8e42f2416d22a1c97dffd9c922663b93ce9df
EsbenNyboeKanda <enj@kanda.dk>
2025-07-31 21:02:57 +0200

    Lerp camera-velocitty

--- camera_controller.gd
+++ camera_controller.gd
@@ -2,11 +2,15 @@ class_name CameraController
 
 extends Camera3D
 
+@export_range(0.001, 1) var lerp_factor: float = 0.1
+
+var target_velocity: Vector3 = Vector3.ZERO
 var velocity: Vector3 = Vector3.ZERO
 
 func set_velocity(new_velocity: Vector3) -> void:
-	velocity = new_velocity
+	target_velocity = new_velocity
 
 func _physics_process(delta: float) -> void:
+	velocity = lerp(velocity, target_velocity, lerp_factor)
 	position.x += velocity.x * delta
 	position.y += velocity.y * delta
