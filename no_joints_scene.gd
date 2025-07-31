d752813a673458cabbff4aec4a6690d793d0dd3a
EsbenNyboeKanda <enj@kanda.dk>
2025-07-31 16:43:13 +0200

    Clean slate

--- no_joints_scene.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-@onready var swing_mount: RigidBody2D = $SwingMount
-@export var torque_amount: float = 1
-
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("swing"):
-		print("torque!")
-		swing_mount.apply_torque(torque_amount * delta)
