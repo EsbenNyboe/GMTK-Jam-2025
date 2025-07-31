aa8f06d0f01451873bbcbcb8cc705d684a42534a
EsbenNyboeKanda <enj@kanda.dk>
2025-07-31 17:55:44 +0200

    Apply torque in bursts

--- main_scene.gd
+++ main_scene.gd
@@ -5,6 +5,11 @@ extends Node3D
 @onready var swing_mount: RigidBody3D = $SwingMount
 @onready var swing_seat: RigidBody3D = $SwingSeat
 
+@export var burst_duration: float = 0.2;
+
+var time_since_last_burst: float = 100000
+
+
 var start_height: float
 var is_above_start_height: bool
 var time_since_last_swing: float = 0
@@ -13,11 +18,16 @@ func _ready() -> void:
 	start_height = swing_seat.position.y + 0.1
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("swing"):
+	time_since_last_burst += delta
+	if time_since_last_burst < burst_duration:
 		swing_mount.apply_torque(Vector3.BACK * torque_amount)
+	
+	if Input.is_action_just_pressed("swing"):
+		time_since_last_burst = 0
 
 	if Input.is_action_pressed("swing"):
 		swing_seat.apply_force(Vector3.RIGHT * force_amount)
+	
 	
 	time_since_last_swing += delta
 	if swing_seat.position.y > start_height:
