e42a894b1e0f71f2b5b9d6caa3338e73a271d52f
EsbenNyboeKanda <enj@kanda.dk>
2025-07-31 22:38:18 +0200

    Fix scene-reload logic

--- jumping_player.gd
+++ jumping_player.gd
@@ -1,5 +1,8 @@
 extends RigidBody3D
 
 func _process(delta: float) -> void:
-	if position.y < -10:
-		get_tree().reload_current_scene()
+	if position.y < -1:
+		print("game over")
+		var tween = create_tween()
+		tween.tween_interval(2)
+		tween.tween_callback(get_tree().reload_current_scene)
