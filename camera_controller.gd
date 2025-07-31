2d0cb7fdc954fb39de2f3ae1392dc9943592b3f4
EsbenNyboeKanda <enj@kanda.dk>
2025-07-31 22:04:13 +0200

    lerp camera more over time

--- camera_controller.gd
+++ camera_controller.gd
@@ -2,15 +2,26 @@ class_name CameraController
 
 extends Camera3D
 
-@export_range(0.001, 1) var lerp_factor: float = 0.1
+@export_range(0.001, 1) var lerp_factor_start: float = 0.1
+@export_range(0.001, 1) var lerp_factor_end: float = 0.1
+@export var lerp_trajectory_duration: float = 1
+var is_lerping: bool = false
+var time_since_start_lerping: float = 0
 
 var target_velocity: Vector3 = Vector3.ZERO
 var velocity: Vector3 = Vector3.ZERO
 
 func set_velocity(new_velocity: Vector3) -> void:
 	target_velocity = new_velocity
+	is_lerping = true
 
 func _physics_process(delta: float) -> void:
-	velocity = lerp(velocity, target_velocity, lerp_factor)
+	if !is_lerping:
+		return
+	
+	time_since_start_lerping += delta
+	var current_lerp_factor = lerp(lerp_factor_start, lerp_factor_end, time_since_start_lerping / lerp_trajectory_duration)
+	current_lerp_factor = clampf(current_lerp_factor, lerp_factor_start, lerp_factor_end)
+	velocity = lerp(velocity, target_velocity, current_lerp_factor)
 	position.x += velocity.x * delta
 	position.y += velocity.y * delta
--- jumping_player.gd
+++ jumping_player.gd
@@ -1,5 +1,5 @@
 extends RigidBody3D
 
 func _process(delta: float) -> void:
-	if position.y < -10:
+	if position.y < -100:
 		get_tree().reload_current_scene()
