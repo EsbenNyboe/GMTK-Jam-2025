805cbf5eb5da4f39deaab3b11ffa9e1996296818
EsbenNyboeKanda <enj@kanda.dk>
2025-07-31 17:04:07 +0200

    Decrease torque amount

--- main_scene.gd
+++ main_scene.gd
@@ -3,15 +3,6 @@ extends Node3D
 @export var torque_amount: float = 10
 @onready var swing_mount: RigidBody3D = $SwingMount
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("swing"):
 		swing_mount.apply_torque(Vector3.BACK * torque_amount)
