ba6c6d2c90921f26a9a5a8ad4b0d49f3782db2e7
EsbenNyboeKanda <enj@kanda.dk>
2025-08-01 16:41:03 +0200

    Make camera lerp faster over time

--- camera_controller.gd
+++ camera_controller.gd
@@ -2,15 +2,23 @@ class_name CameraController
 
 extends Camera3D
 
-@export_range(0.001, 1) var lerp_factor: float = 0.1
+@export_range(0.001, 1) var lerp_factor_start: float = 0.1
+@export_range(0.001, 1) var lerp_factor_end: float = 1
+@export var lerp_timer_duration: float = 10
+var lerp_timer: float = 0
 
 var target_velocity: Vector3 = Vector3.ZERO
 var velocity: Vector3 = Vector3.ZERO
 
-func set_velocity(new_velocity: Vector3) -> void:
+func set_velocity(new_velocity: Vector3, new_target: Vector3) -> void:
 	target_velocity = new_velocity
 
 func _physics_process(delta: float) -> void:
-	velocity = lerp(velocity, target_velocity, lerp_factor)
+	lerp_timer += delta
+	lerp_timer = clampf(lerp_timer, 0, 1)
+	
+	var current_lerp = lerp(lerp_factor_start, lerp_factor_end, lerp_timer / lerp_timer_duration)
+	
+	velocity = lerp(velocity, target_velocity, current_lerp)
 	position.x += velocity.x * delta
 	position.y += velocity.y * delta
--- main_scene.gd
+++ main_scene.gd
@@ -45,7 +45,7 @@ func _process(delta: float) -> void:
 		start_screen.queue_free()
 	
 	if is_jumping:
-		camera_controller.set_velocity(jumping_player_instance.linear_velocity)
+		camera_controller.set_velocity(jumping_player_instance.linear_velocity, jumping_player_instance.global_position)
 		return
 
 	if Input.is_action_just_pressed("jump"):
