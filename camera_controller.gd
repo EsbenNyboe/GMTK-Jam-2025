5451be4d47d4cb0b7d0a5d7d9e52700521499c1c
EsbenNyboeKanda <enj@kanda.dk>
2025-08-01 17:27:08 +0200

    Fix camera logic: Make catch-up correctly

--- camera_controller.gd
+++ camera_controller.gd
@@ -6,8 +6,6 @@ extends Camera3D
 @export_range(0.001, 1) var lerp_factor_end: float = 1
 @export var lerp_timer_duration: float = 10
 
-@export var catch_up_acceleration: float = 0.1
-
 var lerp_timer: float = 0
 
 var target_velocity: Vector3 = Vector3.ZERO
@@ -22,16 +20,18 @@ func set_velocity(new_velocity: Vector3, new_target_position: Vector3) -> void:
 	target_position = new_target_position
 
 func _physics_process(delta: float) -> void:
+	if !JumpingPlayer.instance:
+		return
+
 	lerp_timer += delta
-	lerp_timer = clampf(lerp_timer, 0, 1)
+	lerp_timer = clampf(lerp_timer, 0, lerp_timer_duration)
 	
 	var current_lerp = lerp(lerp_factor_start, lerp_factor_end, lerp_timer / lerp_timer_duration)
 	velocity = lerp(velocity, target_velocity, current_lerp)
 	
-	var catch_up_vector = Vector2.ZERO
-	catch_up_vector.x = 0 if target_position.x - global_position.x < 1 else catch_up_acceleration if velocity.x > 0 else -catch_up_acceleration
-	catch_up_vector.y = 0 if target_position.y - global_position.y < 1 else catch_up_acceleration if velocity.y > 0 else -catch_up_acceleration
-	catch_up_vector = lerp(Vector2.ZERO, catch_up_vector, lerp_timer)
-	
-	position.x += velocity.x * delta + catch_up_vector.x
-	position.y += velocity.y * delta + catch_up_vector.y
+	var position_from_velocity = position 
+	position_from_velocity.x += velocity.x * delta
+	position_from_velocity.y += velocity.y * delta
+		
+	position.x = lerp(position_from_velocity.x, target_position.x, lerp_timer / lerp_timer_duration)
+	position.y = lerp(position_from_velocity.y, target_position.y, lerp_timer / lerp_timer_duration)
