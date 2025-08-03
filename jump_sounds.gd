5b9466c0271f35f0bccc6b3dea18d1e3b8ccc867
EsbenNyboeKanda <enj@kanda.dk>
2025-08-03 09:56:44 +0200

    Add new wind-sounds

--- jump_sounds.gd
+++ jump_sounds.gd
@@ -8,15 +8,22 @@ extends Node
 var has_jumped: bool = false
 var has_landed: bool = false
 
+var wind_tween: Tween
+
 func _process(delta: float) -> void:
+	wind_tween = create_tween()
+	
 	if !has_jumped && JumpingPlayer.instance:
 		has_jumped = true
 		yell_audio.play()
-		wind_audio.volume_linear = 1
+		wind_tween.tween_interval(1)
+		wind_tween.tween_callback(wind_audio.play)
 	
 	if !has_landed && has_jumped && PlayerRagdoll.instance:
 		has_landed = true
 		yell_audio.stop()
+		wind_tween.stop()
+		wind_audio.stop()
+		wind_audio.volume_linear = 0
 		grunt_audio.play()
 		impact_audio.play()
-		wind_audio.volume_linear = 0
