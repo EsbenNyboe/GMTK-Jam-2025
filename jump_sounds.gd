bd6120dc0cc8a2fd5f233e52c32d618a15e0e63e
EsbenNyboeKanda <enj@kanda.dk>
2025-08-03 08:59:23 +0200

    Add wind sound

--- jump_sounds.gd
+++ jump_sounds.gd
@@ -3,6 +3,7 @@ extends Node
 @onready var yell_audio: AudioStreamPlayer = $Yell_Audio
 @onready var grunt_audio: AudioStreamPlayer = $Grunt_Audio
 @onready var impact_audio: AudioStreamPlayer = $Impact_Audio
+@onready var wind_audio: AudioStreamPlayer = $Wind_Audio
 
 var has_jumped: bool = false
 var has_landed: bool = false
@@ -11,9 +12,11 @@ func _process(delta: float) -> void:
 	if !has_jumped && JumpingPlayer.instance:
 		has_jumped = true
 		yell_audio.play()
+		wind_audio.volume_linear = 1
 	
 	if !has_landed && has_jumped && PlayerRagdoll.instance:
 		has_landed = true
 		yell_audio.stop()
 		grunt_audio.play()
 		impact_audio.play()
+		wind_audio.volume_linear = 0
