f896df4066c574fc7f1d63734d087f8ddf0d7e20
EsbenNyboeKanda <enj@kanda.dk>
2025-08-03 09:13:16 +0200

    Fix score counting forever

--- player_ragdoll.gd
+++ player_ragdoll.gd
@@ -8,4 +8,5 @@ func _ready() -> void:
 	instance = self
 
 func _physics_process(delta: float) -> void:
-	MainScene.instance.set_score(global_position.x)
+	if linear_velocity.length() > 0.1:
+		MainScene.instance.set_score(global_position.x)
